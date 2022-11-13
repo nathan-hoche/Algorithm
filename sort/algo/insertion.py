136ac64387354acc94640366856d71bd330907c3
nathan-hoche <hochenat@gmail.com>
2022-11-13 02:34:42 +0000

    [ADD]: Insertion sort

---
+++ sort/algo/insertion.py
@@ -0,0 +1,41 @@
+import time
+
+class algorithm():
+    def __init__(self) -> None:
+        self.numberList = []
+        self.swap = 0
+        self.check = 0
+        self.time = -1
+    
+    def setup(self, numberList: list[list[int]]=None) -> None:
+        if (numberList != None):
+            self.numberList = numberList
+    
+    def countAction(self, isTest: bool=False, swap=False) -> None:
+        if not isTest:
+            if swap:
+                self.swap += 1
+            self.check += 1
+  
+    def run(self, isTest: bool=False) -> None:
+        if not isTest:
+            self.time = time.time()
+        
+            for i in range(len(self.numberList) -1):
+                self.countAction()
+                j = i + 1
+                switch = 0
+                while (j + switch >= 0 and self.numberList[i + switch] > self.numberList[j + switch]):
+                    self.numberList[i + switch], self.numberList[j + switch] = self.numberList[j + switch], self.numberList[i + switch]
+                    self.countAction(swap=True)
+                    switch -= 1
+                self.countAction()
+
+            self.time = time.time() - self.time
+
+    def print(self, isTest: bool=False) -> None:
+        if not isTest:
+            print(self.numberList)
+            print("Swap: ", self.swap)
+            print("Check: ", self.check)
+            print("Time: ", self.time)
