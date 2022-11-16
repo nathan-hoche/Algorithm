a2209dc7990395299af37eeebe4ae68859d55c2b
nathan-hoche <hochenat@gmail.com>
2022-11-16 16:10:20 +0000

    [ADD]: Sequence search sorted system

---
+++ sequence-search-sorted/tester.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import importlib
+import json
+
+######## Loading Part ########
+
+def load_algorithm(filename: str) -> object:
+    try:
+        sys.path.append(os.getcwd() + "/algo/")
+        fd = importlib.import_module(filename.replace(".py", ""))
+        print("Load: ", filename, "found.")
+    except Exception as e:
+        print("ERROR: File", filename, "not found.")
+        print(e)
+        exit(0)
+    try:
+        classfd = fd.algorithm()
+        print("Load: Class found. -> ", type(classfd))
+        ####### Check if sample fonction is set
+        classfd.setup()
+        classfd.run(True)
+        classfd.print(True)
+        #######################################
+    except Exception as e:
+        print("ERROR: class/method crashed")
+        print(e)
+        exit(0)
+    return classfd
+
+def load_variable(filename: str) -> list[int]:
+    try:
+        with open("json/" + filename, "r") as f:
+            data = json.load(f)
+            print("Load: ", filename, "found.")
+    except Exception as e:
+        print("ERROR: File", filename, "not found.")
+        print(e)
+        exit(0)
+    if type(data) == dict:
+        return data
+    else:
+        print("ERROR: Invalid type of variable.")
+        exit(0)
+
+######## Main Part ########
+
+def main():
+    if len(sys.argv) != 3:
+        print("ERROR: Invalid number of arguments.\n Usage: python3 tester.py [algo] [jsonfile]")
+        return
+    algo = load_algorithm(sys.argv[1])
+    info = load_variable(sys.argv[2])
+    algo.setup(info)
+    algo.run()
+    algo.print()
+
+main()
