81c44d21b0de8d16e82fac7b63f4fb6e221a6d08
nathan-hoche <hochenat@gmail.com>
2022-11-16 16:20:46 +0000

    [ADD]: binary search

---
+++ sequence-search-sorted/algo/binary.py
@@ -0,0 +1,38 @@
+import time
+
+class algorithm():
+    def __init__(self) -> None:
+        self.list = []
+        self.objectif = -1
+        self.found = False
+        self.time = -1
+    
+    def setup(self, info: dict=None) -> None:
+        if (info != None):
+            self.list = info["list"]
+            self.objectif = info["objectif"]
+  
+    def run(self, isTest: bool=False) -> None:
+        if not isTest:
+            self.time = time.time()
+            
+            x = 0
+            y = len(self.list) - 1
+            while x <= y:
+                m = int((x + y) / 2)
+                if self.list[m] == self.objectif:
+                    self.found = True
+                    break
+                if self.list[m] < self.objectif:
+                    x = m + 1
+                else:
+                    y = m - 1
+            self.time = time.time() - self.time
+
+    def print(self, isTest: bool=False) -> None:
+        if not isTest:
+            #print("List: ", self.list)
+            print("Size of list: ", len(self.list))
+            print("Objectif: ", self.objectif)
+            print("Found: ", self.found)
+            print("Time: ", self.time)
